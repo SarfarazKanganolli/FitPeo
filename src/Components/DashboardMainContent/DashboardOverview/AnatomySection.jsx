8cd82e913ef4c2e6e704cb656cf4f58d9526b32a
sarfaraz kdsc <sarfarazkanganolli@gmail.com>
2025-05-28 00:43:00 +0530

    Anatomy commit

--- src/Components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
+++ src/Components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
@@ -4,7 +4,7 @@ import Body from "@/assets/body.png"
 import Arrow from "@/assets/arrow.svg"
 
 import organs from '@/data/anatomy.js'
-import { Progress } from "@/components/ui/progress"
+import { Progress } from "../../ui/avatar"
 
 const AnatomySection = () => {
 
