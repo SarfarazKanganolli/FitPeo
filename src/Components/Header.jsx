48b68868c80117c28552715795a25de8a22d5341
sarfaraz kdsc <sarfarazkanganolli@gmail.com>
2025-05-28 00:41:13 +0530

    Header Avatar commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "../Components/ui/avatar"
 import Hamburger from "@/assets/hamburger.svg"
 import Cross from "@/assets/cross.svg"
 
